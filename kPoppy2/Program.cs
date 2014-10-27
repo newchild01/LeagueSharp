d5d73a2b3cbc3c021d42b13a3ff26c492c756027
newchild01 <dragonfighter1234567@gmail.com>
2014-10-27 10:04:03 +0100

    Update Program.cs

--- kPoppy2/Program.cs
+++ kPoppy2/Program.cs
@@ -204,7 +204,7 @@ namespace kPoppy2
         {
             foreach (var hero in ObjectManager.Get<Obj_AI_Hero>().Where(hero => hero.IsValidTarget(_e.Range)))
             {
-                var dmg = Player.GetDamageSpell(hero, SpellSlot.E) + Player.GetDamageSpell(hero, SpellSlot.Q);
+                var dmg = (int)Player.GetDamageSpell(hero, SpellSlot.E) + (int)Player.GetDamageSpell(hero, SpellSlot.Q);
                 if (_e.IsReady() && hero.Distance(ObjectManager.Player) <= _e.Range && dmg >= hero.Health)
                 {
                     _e.CastOnUnit(hero, true);
